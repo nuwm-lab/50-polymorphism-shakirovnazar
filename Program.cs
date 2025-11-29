0479800fd1760388acc5ac62177650ad46954ed0
Nazarii Shakirov <shakirov_ak24@nuwm.edu.ua>
2025-11-30 00:46:12 +0200

    Refactor to support 2D and 3D matrix operations

--- Program.cs
+++ Program.cs
@@ -1,88 +1,144 @@
 using System;
 
-class Figure
+public class Matrix2D
 {
+    protected int[,] data = new int[3, 3];
+    protected Random rnd = new Random();
+
     public virtual void Input()
     {
-        Console.WriteLine("Фігура: немає даних.");
+        Console.WriteLine("Введення матриці 3x3:");
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($"[{i},{j}] = ");
+                while (!int.TryParse(Console.ReadLine(), out data[i, j]))
+                    Console.Write("Помилка. Введіть число: ");
+            }
     }
 
-    public virtual double Area()
+    public virtual void RandomFill()
     {
-        return 0;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                data[i, j] = rnd.Next(-50, 51);
     }
 
     public virtual void Show()
     {
-        Console.WriteLine("Площа = " + Area());
+        Console.WriteLine("\nМатриця 2D:");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+                Console.Write($"{data[i, j],4}");
+            Console.WriteLine();
+        }
+    }
+
+    public virtual int MinElement()
+    {
+        int min = data[0, 0];
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (data[i, j] < min)
+                    min = data[i, j];
+
+        return min;
     }
 }
 
-class Rectangle : Figure
+
+public class Matrix3D : Matrix2D
 {
-    double w, h;
+    private int[,,] data3d = new int[3, 3, 3];
 
     public override void Input()
     {
-        Console.Write("Ширина: ");
-        w = Convert.ToDouble(Console.ReadLine());
-        Console.Write("Висота: ");
-        h = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введення матриці 3x3x3:");
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                for (int k = 0; k < 3; k++)
+                {
+                    Console.Write($"[{i},{j},{k}] = ");
+                    while (!int.TryParse(Console.ReadLine(), out data3d[i, j, k]))
+                        Console.Write("Помилка. Введіть число: ");
+                }
     }
 
-    public override double Area()
+    public override void RandomFill()
     {
-        return w * h;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                for (int k = 0; k < 3; k++)
+                    data3d[i, j, k] = rnd.Next(-50, 51);
     }
 
     public override void Show()
     {
-        Console.WriteLine("Прямокутник. Площа = " + Area());
+        Console.WriteLine("\nМатриця 3D:");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"Шар Z={i}:");
+            for (int j = 0; j < 3; j++)
+            {
+                for (int k = 0; k < 3; k++)
+                    Console.Write($"{data3d[i, j, k],4}");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public override int MinElement()
+    {
+        int min = data3d[0, 0, 0];
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                for (int k = 0; k < 3; k++)
+                    if (data3d[i, j, k] < min)
+                        min = data3d[i, j, k];
+
+        return min;
     }
 }
 
-class Circle : Figure
+
+public class Program
 {
-    double r;
-
-    public override void Input()
+    public static void Main()
     {
-        Console.Write("Радіус: ");
-        r = Convert.ToDouble(Console.ReadLine());
-    }
-
-    public override double Area()
-    {
-        return Math.PI * r * r;
-    }
-
-    public override void Show()
-    {
-        Console.WriteLine("Коло. Площа = " + Area());
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("1 - Прямокутник");
-        Console.WriteLine("2 - Коло");
+        Console.WriteLine("1 – Матриця 2D");
+        Console.WriteLine("2 – Матриця 3D");
         Console.Write("Ваш вибір: ");
 
-        char user = Console.ReadKey().KeyChar;
+        char choice = Console.ReadKey().KeyChar;
         Console.WriteLine();
 
-        Figure fig;   // вказівник на базовий клас
+        Matrix2D matrix; // поліморфізм через базовий клас
 
-        if (user == '1')
-            fig = new Rectangle();
+        if (choice == '1')
+            matrix = new Matrix2D();
         else
-            fig = new Circle();
+            matrix = new Matrix3D();
 
-        fig.Input();
-        fig.Show();
+        Console.WriteLine("\n(1) Ввести вручну");
+        Console.WriteLine("(2) Заповнити випадково");
+        Console.Write("Ваш вибір: ");
+        char mode = Console.ReadKey().KeyChar;
+        Console.WriteLine();
 
-        Console.ReadKey();
+        if (mode == '1')
+            matrix.Input();
+        else
+            matrix.RandomFill();
+
+        matrix.Show();
+        Console.WriteLine($"\nМінімальний елемент = {matrix.MinElement()}");
+
+        Console.WriteLine("\nГотово.");
     }
 }
