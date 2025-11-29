1d30a7866d86b5b6ff34a8cab322b0f376788cb5
Nazarii Shakirov <shakirov_ak24@nuwm.edu.ua>
2025-11-30 00:43:19 +0200

    Refactor Figure classes and main program structure

--- Program.cs
+++ Program.cs
@@ -1,18 +1,88 @@
-﻿using System;
+using System;
 
-namespace LabWork
+class Figure
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
+    public virtual void Input()
     {
-        static void Main(string[] args)
-        {
-            
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Фігура: немає даних.");
+    }
+
+    public virtual double Area()
+    {
+        return 0;
+    }
+
+    public virtual void Show()
+    {
+        Console.WriteLine("Площа = " + Area());
+    }
+}
+
+class Rectangle : Figure
+{
+    double w, h;
+
+    public override void Input()
+    {
+        Console.Write("Ширина: ");
+        w = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Висота: ");
+        h = Convert.ToDouble(Console.ReadLine());
+    }
+
+    public override double Area()
+    {
+        return w * h;
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine("Прямокутник. Площа = " + Area());
+    }
+}
+
+class Circle : Figure
+{
+    double r;
+
+    public override void Input()
+    {
+        Console.Write("Радіус: ");
+        r = Convert.ToDouble(Console.ReadLine());
+    }
+
+    public override double Area()
+    {
+        return Math.PI * r * r;
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine("Коло. Площа = " + Area());
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("1 - Прямокутник");
+        Console.WriteLine("2 - Коло");
+        Console.Write("Ваш вибір: ");
+
+        char user = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Figure fig;   // вказівник на базовий клас
+
+        if (user == '1')
+            fig = new Rectangle();
+        else
+            fig = new Circle();
+
+        fig.Input();
+        fig.Show();
+
+        Console.ReadKey();
     }
 }
